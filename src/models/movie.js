4681132b3eba88c4d46db4f04e8f1af8dc14bcf6
NikolaIvanov0821 <ni.ivanov2008@gmail.com>
2025-01-25 15:41:41 +0200

    add movie model

---
+++ src/models/movie.js
@@ -0,0 +1,13 @@
+export class Movie {
+    constructor(id, title, category, genre, director, year, imageURL, rating, description) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.genre = genre;
+        this.director = director;
+        this.year = year;
+        this.imageURL = imageURL;
+        this.rating = rating;
+        this.description = description;
+    }
+}
