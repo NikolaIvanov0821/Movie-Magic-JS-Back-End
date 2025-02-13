b691d044be41d6070c4e745ae0a25cab847c2a5a
NikolaIvanov0821 <ni.ivanov2008@gmail.com>
2025-02-13 20:56:57 +0200

    render register page

---
+++ src/controllers/authController.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const authController = Router();
+
+authController.get('/register', (req, res) => {
+    res.render('register')
+});
+
+export default authController;
--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import homeController from "./controllers/homeController.js";
 import moviesController from "./controllers/movieController.js";
 import castController from "./controllers/castController.js";
+import authController from "./controllers/authController.js";
 
 const router = Router();
 
 router.use(homeController);
 router.use(moviesController);
 router.use(castController);
+router.use(authController);
 
 export default router;
